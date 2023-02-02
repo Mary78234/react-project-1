33844768601f1bc2423548907ec006d3d35dca44
Wang Quanquan (EXT Reply) <q.wang@reply.com>
2023-02-02 11:38:22 +0100

    assignment 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,15 +5,19 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
-  const [saveYear, setYear] = useState(2022);
+  const years = [2023, 2022, 2021, 2020, 2019];
+  const [selectedYear, setYear] = useState(2021);
   const sendYear = (year) => {
     setYear(year);
-    console.log(saveYear);
   };
   return (
-    <div className="expenses">
-      <ExpensesFilter onSendYear={sendYear} />
-      <Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          years={years}
+          selected={selectedYear}
+          onSendYear={sendYear}
+        />
         {props.expenses.map((ex) => (
           <ExpenseItem
             key={ex.id}
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,25 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-  const [saveYear, setYear] = useState(2022);
-  const years = [2022, 2021, 2020, 2019];
-  const listYears = years.map((year) => (
+  const listYears = props.years.map((year) => (
     <option key={year.toString()} value={year}>
       {year}
     </option>
   ));
 
-  const selectedYear = (event) => {
-    setYear(event.target.value);
+  const changeSelectedYear = (event) => {
     props.onSendYear(event.target.value);
   };
 
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
-        <label>Filter by year {saveYear}</label>
-        <select onChange={selectedYear} value={saveYear}>
+        <label>Filter by year</label>
+        <select onChange={changeSelectedYear} value={props.selected}>
           {listYears}
         </select>
       </div>
